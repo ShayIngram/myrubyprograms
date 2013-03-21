c3ca12e8bf679190a575fa2da061dfc55b93d79f
Shanelle Ingram <shanelle.ingram1@gmail.com>
2013-03-20 21:35:30 -0700

    Add propmt to file_overwrite.rb and added new file to repo

--- file_overwrite.rb
+++ file_overwrite.rb
@@ -2,17 +2,24 @@
 Ruby program read a file. Then, finds "word" and replaces it with a given substition.
 The new string overwrites the original contents before the file is closed. 
 =end
+def prompt
+	print '>>'
+end
 
 puts 'Enter a file name:'
-print '>>'
+prompt
 
-input = STDIN.gets.chomp
+input = gets.chomp
+
+puts 'Enter the string to be substituted:'
+prompt
+str = gets.chomp
 
 txt = File.new(input, 'r')
 contents = txt.read()
 
-if contents.include? "word"
-  new_contents = contents.gsub!(/word/, "inserted word")
+if  contents.include? str
+  new_contents = contents.gsub!(str, "inserted word")
 end
 
 File.open(input, 'w'){|f| f.puts new_contents}
