facd14920236a5eed91f2d87cc02c71f39fc00b0
Shanelle Ingram <shanelle.ingram1@gmail.com>
2013-03-21 20:05:46 -0700

    Refactored the program

--- file_overwrite.rb
+++ file_overwrite.rb
@@ -2,17 +2,13 @@
 Ruby program read a file. Then, finds "word" and replaces it with a given substition.
 The new string overwrites the original contents before the file is closed. 
 =end
-def prompt
-	print '>>'
-end
 
 puts 'Enter a file name:'
-prompt
 
 input = gets.chomp
 
 puts 'Enter the string to be substituted:'
-prompt
+
 str = gets.chomp
 
 txt = File.new(input, 'r')
